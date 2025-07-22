6ffe48537088d7ec07ed5b1869ec397b15f3c9de
UTampa2025 <marie.hall@spartans.ut.edu>
2025-07-21 23:56:00 -0400

    Update script.js
    y

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const scene = document.querySelector('a-scene').object3D;
 function loadOven(name) {
   console.log("Loading oven:", name);
  // Replace with your current Netlify site URL if different
-  const url = `https://fluffy-alpaca-ebf82f.netlify.app/${name}.glb`;
+  const url = `https://poetic-faun-6a50b3.netlify.app/${name}.glb`;
   loader.load(
     url,
     function (gltf) {
